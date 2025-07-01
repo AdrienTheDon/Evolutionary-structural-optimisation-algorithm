8b5139d5654991aaa68f33ec8f4a2963cd134747
Crackaudiovisuel <68559609+AdrienTheDon@users.noreply.github.com>
2025-07-01 14:25:32 +0200

    Vectorize sparse matrix assembly

--- eso.py
+++ eso.py
@@ -44,56 +44,79 @@ def finite_temp_direct_sparse(kp_k0, k0, heat_sink_temperature, delta_x, p_vol, 
     conductivity_table[1:-1, 1:-1, 3] = delta_x / ((delta_x / 2) / west + (delta_x / 2) / center)
 
     address = np.arange(height * width).reshape((height, width))
-    rows, cols, data = [], [], []
     B = np.zeros(height * width)
 
-    for i in range(height):
-        for j in range(width):
-            ind_1 = address[i, j]
-            if boundary_conditions[i, j] not in (-2, -3):
-                ind_2 = address[i - 1, j]
-                ind_3 = address[i, j + 1]
-                ind_4 = address[i + 1, j]
-                ind_5 = address[i, j - 1]
-                somme_cond = conductivity_table[i, j].sum()
-            fin = False
-            if boundary_conditions[i, j] in (-2, -3):
-                B[ind_1] = temp[i, j]
-                rows.append(ind_1)
-                cols.append(ind_1)
-                data.append(1)
-                fin = True
-            else:
-                rows.append(ind_1)
-                cols.append(ind_1)
-                data.append(-somme_cond)
-                if boundary_conditions[i, j] == k0:
-                    B[ind_1] -= p_vol * delta_x**2
-            if not fin:
-                if boundary_conditions[i - 1, j] in (-2, -3):
-                    B[ind_1] -= temp[i - 1, j] * conductivity_table[i, j, 0]
-                else:
-                    rows.append(ind_1)
-                    cols.append(ind_2)
-                    data.append(conductivity_table[i, j, 0])
-                if boundary_conditions[i, j + 1] in (-2, -3):
-                    B[ind_1] -= temp[i, j + 1] * conductivity_table[i, j, 1]
-                else:
-                    rows.append(ind_1)
-                    cols.append(ind_3)
-                    data.append(conductivity_table[i, j, 1])
-                if boundary_conditions[i + 1, j] in (-2, -3):
-                    B[ind_1] -= temp[i + 1, j] * conductivity_table[i, j, 2]
-                else:
-                    rows.append(ind_1)
-                    cols.append(ind_4)
-                    data.append(conductivity_table[i, j, 2])
-                if boundary_conditions[i, j - 1] in (-2, -3):
-                    B[ind_1] -= temp[i, j - 1] * conductivity_table[i, j, 3]
-                else:
-                    rows.append(ind_1)
-                    cols.append(ind_5)
-                    data.append(conductivity_table[i, j, 3])
+    dirichlet_mask = (boundary_conditions == -2) | (boundary_conditions == -3)
+
+    # indices of all cells
+    center_idx = address[1:-1, 1:-1]
+
+    # mask of internal cells (non Dirichlet)
+    internal_mask = ~dirichlet_mask[1:-1, 1:-1]
+
+    # diagonal terms for Dirichlet cells
+    dir_idx = address[dirichlet_mask]
+    rows_dir = dir_idx
+    cols_dir = dir_idx
+    data_dir = np.ones_like(dir_idx, dtype=float)
+    B[dir_idx] = temp[dirichlet_mask]
+
+    # diagonal terms for internal cells
+    diag_idx = center_idx[internal_mask]
+    somme_cond = conductivity_table[1:-1, 1:-1].sum(axis=2)[internal_mask]
+    rows_diag = diag_idx
+    cols_diag = diag_idx
+    data_diag = -somme_cond
+
+    k0_mask = boundary_conditions[1:-1, 1:-1][internal_mask] == k0
+    B[diag_idx[k0_mask]] -= p_vol * delta_x ** 2
+
+    # neighbour indices
+    north_idx = address[:-2, 1:-1][internal_mask]
+    east_idx = address[1:-1, 2:][internal_mask]
+    south_idx = address[2:, 1:-1][internal_mask]
+    west_idx = address[1:-1, :-2][internal_mask]
+
+    north_bc = boundary_conditions[:-2, 1:-1][internal_mask]
+    east_bc = boundary_conditions[1:-1, 2:][internal_mask]
+    south_bc = boundary_conditions[2:, 1:-1][internal_mask]
+    west_bc = boundary_conditions[1:-1, :-2][internal_mask]
+
+    cond_n = conductivity_table[1:-1, 1:-1, 0][internal_mask]
+    cond_e = conductivity_table[1:-1, 1:-1, 1][internal_mask]
+    cond_s = conductivity_table[1:-1, 1:-1, 2][internal_mask]
+    cond_w = conductivity_table[1:-1, 1:-1, 3][internal_mask]
+
+    # masks for neighbour Dirichlet
+    mask_n_dir = (north_bc == -2) | (north_bc == -3)
+    mask_e_dir = (east_bc == -2) | (east_bc == -3)
+    mask_s_dir = (south_bc == -2) | (south_bc == -3)
+    mask_w_dir = (west_bc == -2) | (west_bc == -3)
+
+    B[diag_idx[mask_n_dir]] -= temp[:-2, 1:-1][internal_mask][mask_n_dir] * cond_n[mask_n_dir]
+    B[diag_idx[mask_e_dir]] -= temp[1:-1, 2:][internal_mask][mask_e_dir] * cond_e[mask_e_dir]
+    B[diag_idx[mask_s_dir]] -= temp[2:, 1:-1][internal_mask][mask_s_dir] * cond_s[mask_s_dir]
+    B[diag_idx[mask_w_dir]] -= temp[1:-1, :-2][internal_mask][mask_w_dir] * cond_w[mask_w_dir]
+
+    rows_north = diag_idx[~mask_n_dir]
+    cols_north = north_idx[~mask_n_dir]
+    data_north = cond_n[~mask_n_dir]
+
+    rows_east = diag_idx[~mask_e_dir]
+    cols_east = east_idx[~mask_e_dir]
+    data_east = cond_e[~mask_e_dir]
+
+    rows_south = diag_idx[~mask_s_dir]
+    cols_south = south_idx[~mask_s_dir]
+    data_south = cond_s[~mask_s_dir]
+
+    rows_west = diag_idx[~mask_w_dir]
+    cols_west = west_idx[~mask_w_dir]
+    data_west = cond_w[~mask_w_dir]
+
+    rows = np.concatenate([rows_dir, rows_diag, rows_north, rows_east, rows_south, rows_west])
+    cols = np.concatenate([cols_dir, cols_diag, cols_north, cols_east, cols_south, cols_west])
+    data = np.concatenate([data_dir, data_diag, data_north, data_east, data_south, data_west])
 
     A = sparse.csr_matrix((data, (rows, cols)), shape=(height * width, height * width))
     T = spsolve(A, B)
